168c945f1167026cbc5d7a7e80e3b120be2d826a
AchiCatty <bulsago2001@google.com>
2020-12-29 10:27:28 +0900

    .

--- FinalProject/User/UserAPI/UserForm/InputStorageForm.Designer.cs
+++ FinalProject/User/UserAPI/UserForm/InputStorageForm.Designer.cs
@@ -31,23 +31,18 @@ namespace UserForm
         {
             this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
             this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
-            this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
-            this.layoutControlItem = new DevExpress.XtraLayout.LayoutControlItem();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
-            this.layoutControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem)).BeginInit();
             this.SuspendLayout();
             // 
             // layoutControl1
             // 
-            this.layoutControl1.Controls.Add(this.pictureEdit1);
             this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.layoutControl1.Name = "layoutControl1";
             this.layoutControl1.Root = this.Root;
-            this.layoutControl1.Size = new System.Drawing.Size(1028, 809);
+            this.layoutControl1.Size = new System.Drawing.Size(1028, 971);
             this.layoutControl1.TabIndex = 0;
             this.layoutControl1.Text = "layoutControl1";
             // 
@@ -55,44 +50,21 @@ namespace UserForm
             // 
             this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
             this.Root.GroupBordersVisible = false;
-            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
-            this.layoutControlItem});
             this.Root.Name = "Root";
-            this.Root.Size = new System.Drawing.Size(1028, 809);
+            this.Root.Size = new System.Drawing.Size(1028, 971);
             this.Root.TextVisible = false;
-            // 
-            // pictureEdit1
-            // 
-            this.pictureEdit1.EditValue = global::UserForm.Properties.Resources.storage_box_svgrepo_com;
-            this.pictureEdit1.Location = new System.Drawing.Point(12, 12);
-            this.pictureEdit1.Name = "pictureEdit1";
-            this.pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
-            this.pictureEdit1.Size = new System.Drawing.Size(1004, 785);
-            this.pictureEdit1.StyleController = this.layoutControl1;
-            this.pictureEdit1.TabIndex = 4;
-            // 
-            // layoutControlItem
-            // 
-            this.layoutControlItem.Control = this.pictureEdit1;
-            this.layoutControlItem.Location = new System.Drawing.Point(0, 0);
-            this.layoutControlItem.Name = "layoutControlItem";
-            this.layoutControlItem.Size = new System.Drawing.Size(1008, 789);
-            this.layoutControlItem.TextSize = new System.Drawing.Size(0, 0);
-            this.layoutControlItem.TextVisible = false;
             // 
             // InputStorageForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1028, 809);
+            this.ClientSize = new System.Drawing.Size(1028, 971);
             this.Controls.Add(this.layoutControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "InputStorageForm";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
-            this.layoutControl1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -100,9 +72,7 @@ namespace UserForm
         #endregion
 
         private DevExpress.XtraLayout.LayoutControl layoutControl1;
-        private DevExpress.XtraEditors.PictureEdit pictureEdit1;
         private DevExpress.XtraLayout.LayoutControlGroup Root;
-        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem;
     }
 }
 
